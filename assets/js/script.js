509508dc0277bcf2f73779a00392e4ec70ab91ee
Memmedliorxan <orkhanama@code.edu.az>
2022-04-21 21:25:52 +0400

    first commir

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let menu = document.getElementById('menu');
+let sidebar = document.getElementById('sidebar');
+let close = document.getElementById('close');
+
+
+menu.addEventListener('click',function(){
+    sidebar.classList.add('active');
+})
+
+close.addEventListener('click',function(){
+    sidebar.classList.remove('active');
+})
